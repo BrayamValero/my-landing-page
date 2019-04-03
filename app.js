fe7f64c3c7bc6384191d234cdb9ea030812ca5ce
Brayam <bvalerop@gmail.com>
2019-04-03 08:50:40 -0400

    minor changes

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
+    .not('[href="#carouselExampleIndicators"]')
     .click(function(event) {
       // On-page links
       if (
